82846a4ff1a44a5db979a086ce736880c513ad0b
Dmitry Yuroskulov <43011788+DmitryYuroskulov@users.noreply.github.com>
2021-05-04 17:19:23 +0600

    clearing

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,9 @@
         id="app-layout"
         class="d-flex align-items-center justify-content-center"
     >
-        <div class="TodoContainer px-3 py-2">
-            <TodoFoldersList />
-            <TodoFolderItemsList v-show="false" />
-        </div>
+        app
     </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, onMounted, reactive } from 'vue'
-
-import TodoFoldersList from '@/components/TodoFoldersList/index.vue'
-import TodoFolderItemsList from '@/components/TodoFolderItemsList/index.vue'
-
-import { getFolders, TodoFoldersListType } from '@/api/todo-folders'
-
-export default defineComponent({
-    components: { TodoFoldersList, TodoFolderItemsList },
-    setup() {
-        const componentState = reactive({
-            folders: [] as TodoFoldersListType,
-        })
-
-        const getData = async () => {
-            componentState.folders = await getFolders()
-        }
-
-        onMounted(getData)
-    },
-})
-</script>
 
 <style lang="scss">
 @font-face {
@@ -52,16 +25,12 @@ html {
     font-family: 'OpenSans';
 }
 
+body {
+    background-color: #f3f9fd;
+}
+
 #app-layout {
     width: 100vw;
     min-height: 100vh;
-}
-
-.TodoContainer {
-    width: 100%;
-    max-width: 800px;
-
-    background: rgb(235, 235, 235);
-    border-radius: 5px;
 }
 </style>
